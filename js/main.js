a09bcca96d15d89d1e22531536a8b518354d24c2
Vlad Golubtsov <v.golubtsov@bvblogic.com>
2020-04-25 23:38:44 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('js/sw.js');
+    navigator.serviceWorker.register('./js/sw.js');
   }
 };
